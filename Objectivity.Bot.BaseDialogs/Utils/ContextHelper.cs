083dcfacbe8b83982c63283e46f6e441a8ac92e7
Jacek Gratka <jgratka@objectivity.co.uk>
2017-08-18 22:32:00 +0200

    Add context helper

---
+++ Objectivity.Bot.BaseDialogs/Utils/ContextHelper.cs
@@ -0,0 +1,49 @@
+﻿namespace Objectivity.Bot.BaseDialogs.Utils
+{
+    using System;
+    using Microsoft.Bot.Builder.Dialogs;
+
+    public static class ContextHelper
+    {
+        public static T GetValueFromContext<T>(IDialogContext context, string key)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            T result;
+            if (!context.UserData.TryGetValue(key, out result))
+            {
+                return default(T);
+            }
+
+            return result;
+        }
+
+        public static bool TryGetValueFromContext<T>(IDialogContext context, string key, out T value)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (context.UserData.TryGetValue(key, out value))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static void SetValueIntoContext<T>(IDialogContext context, string key, T value)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            context.UserData.SetValue(key, value);
+        }
+    }
+}
