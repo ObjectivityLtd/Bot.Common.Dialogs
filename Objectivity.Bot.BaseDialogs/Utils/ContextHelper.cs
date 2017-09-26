ca1edfbf29cb2a184e28cd9e449f366e1edf7549
Jacek Gratka <jgratka@objectivity.co.uk>
2017-09-26 15:18:15 +0200

    Add extensions to store values in the bot state
    Make ContextHelper's methods as an extensions

--- Objectivity.Bot.BaseDialogs/Utils/ContextHelper.cs
+++ Objectivity.Bot.BaseDialogs/Utils/ContextHelper.cs
@@ -1,11 +1,13 @@
 ﻿namespace Objectivity.Bot.BaseDialogs.Utils
 {
     using System;
+    using System.Threading.Tasks;
     using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Connector;
 
     public static class ContextHelper
     {
-        public static T GetValueFromContext<T>(IDialogContext context, string key)
+        public static T GetValueFromContext<T>(this IDialogContext context, string key)
         {
             if (context == null)
             {
@@ -21,7 +23,7 @@
             return result;
         }
 
-        public static bool TryGetValueFromContext<T>(IDialogContext context, string key, out T value)
+        public static bool TryGetValueFromContext<T>(this IDialogContext context, string key, out T value)
         {
             if (context == null)
             {
@@ -36,7 +38,7 @@
             return false;
         }
 
-        public static void SetValueIntoContext<T>(IDialogContext context, string key, T value)
+        public static void SetValueIntoContext<T>(this IDialogContext context, string key, T value)
         {
             if (context == null)
             {
@@ -45,5 +47,42 @@
 
             context.UserData.SetValue(key, value);
         }
+
+        public static async Task SetValueIntoState<T>(this IDialogContext context, string key, T value)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            using (StateClient stateClient = context.Activity.GetStateClient())
+            {
+
+                IBotState chatbotState = stateClient.BotState;
+                BotData chatbotData = await chatbotState.GetUserDataAsync(
+                    context.Activity.ChannelId, context.Activity.From.Id);
+
+                chatbotData.SetProperty(key, value);
+                await chatbotState.SetUserDataAsync(context.Activity.ChannelId, context.Activity.From.Id, chatbotData);
+            }
+        }
+
+        public static async Task<T> GetValueFromState<T>(this IDialogContext context, string key)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            using (StateClient stateClient = context.Activity.GetStateClient())
+            {
+                IBotState chatbotState = stateClient.BotState;
+                BotData chatbotData =
+                    await chatbotState.GetUserDataAsync(context.Activity.ChannelId, context.Activity.From.Id);
+
+                return chatbotData.GetProperty<T>(key);
+            }
+        }
+
     }
 }
