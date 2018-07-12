a72eeafa4912c5f9fb51fb8450ac8f0bf59e5dd0
Jacek Kolonko <jkolonko@objectivity.co.uk>
2018-07-12 14:10:30 +0200

    Removed bot-specific intents

--- Objectivity.Bot.BaseDialogs/LuisApp/Intents.cs
+++ Objectivity.Bot.BaseDialogs/LuisApp/Intents.cs
@@ -2,39 +2,8 @@
 {
     public static class Intents
     {
-        public const string Abort = nameof(Abort);
-
-        public const string AddFloors = nameof(AddFloors);
-
-        // public const string PrinterFinder = nameof(Intents.PrinterFinder);
-        public const string BusTimetable = nameof(BusTimetable);
-
-        public const string Bye = nameof(Bye);
-
-        public const string FindPerson = nameof(FindPerson);
-
-        public const string FindRoom = nameof(FindRoom);
-
-        public const string FootballTable = nameof(FootballTable);
-
         public const string GetDate = nameof(GetDate);
 
-        public const string Hello = nameof(Hello);
-
-        public const string Help = nameof(Help);
-
-        public const string JiraTask = nameof(JiraTask);
-
-        public const string Link = nameof(Link);
-
-        public const string Maps = nameof(Maps);
-
         public const string None = nameof(None);
-
-        public const string ThankYou = nameof(ThankYou);
-
-        public const string TrainTimetable = nameof(TrainTimetable);
-
-        public const string WiFi = nameof(WiFi);
     }
 }
